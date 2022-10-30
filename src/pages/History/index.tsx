3be6a44e325ed4899f5287c5cb9a65520d772368
matheuslima99 <maths.soares99@gmail.com>
2022-10-30 04:25:12 -0300

    Pages and Routes - status component

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -21,28 +21,68 @@ export function History() {
               <td>Tarefas</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefas</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Concluido</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefas</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefas</td>
               <td>20 minutos</td>
               <td>Há 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefas</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefas</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefas</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefas</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
