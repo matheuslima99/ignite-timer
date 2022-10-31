dad51673f1af5c6aaf1a399ee5503f64d65bd356
matheuslima99 <maths.soares99@gmail.com>
2022-10-31 02:42:44 -0300

    Forms: typescript in form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,12 +21,18 @@ const newCycleFormValidationScheme = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
 
+type newCycleFormData = zod.infer<typeof newCycleFormValidationScheme>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<newCycleFormData>({
     resolver: zodResolver(newCycleFormValidationScheme),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: newCycleFormData) {
     console.log(data)
   }
 
