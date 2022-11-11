d466cd18eb1d9c2dc07b897693ed128d49a8eeed
matheuslima99 <maths.soares99@gmail.com>
2022-11-11 03:06:25 -0300

    React context- context between routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 
-import { BrowserRouter } from 'react-router-dom'
-
 import { GlobalStyle } from './styles/global'
-
 import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,8 +1,7 @@
 import { useContext, useEffect } from 'react'
-import { CyclesContext } from '../..'
-import { CountdownContainer, Separator } from './styles'
-
 import { differenceInSeconds } from 'date-fns'
+import { CountdownContainer, Separator } from './styles'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
 
 export function Countdown() {
   const {
